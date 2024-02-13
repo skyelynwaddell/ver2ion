fa334b88de35e09683775bd843e7987016d35026
Skye <105599773+skyelynwaddell@users.noreply.github.com>
2024-02-13 14:24:24 -0700

    added good to releases

--- createrelease.py
+++ createrelease.py
@@ -1,18 +1,18 @@
 import fileinput
 
 replaceTexts = {
-    '#COVER_ART':'',
-    '#TITLE':'ver2ion - ',
+    '#COVER_ART':'https://s3.amazonaws.com/gather.fandalism.com/3000x3000%2D1197190%2D%2DD7A2CFEA%2D0A0B%2D4D2D%2D80581A9E82C13913%2D%2D0%2D%2D1696534%2D%2Dver2iongoodprod.9dmbeats.jpg',
+    '#TITLE':'ver2ion - good',
     '#ARTIST':'ver2ion',
-    '#SONG_TITLE':'songtitle',
-    '#RELEASE_DATE':'releasedate',
+    '#SONG_TITLE':'good',
+    '#RELEASE_DATE':'February 18 2024',
 
-    '#SOUNDCLOUD':'sc',
-    '#SPOTIFY':'sptfy',
-    '#ITUNES':'itunes',
-    '#YOUTUBE':'youtube',
-    '#YOUTUBE_MUSIC':'youtubemusic',
-    '#DEEZER':'deezer',
+    '#SOUNDCLOUD':'',
+    '#SPOTIFY':'',
+    '#ITUNES':'',
+    '#YOUTUBE':'',
+    '#YOUTUBE_MUSIC':'',
+    '#DEEZER':'',
 }
 
 for line in fileinput.input('template.html', inplace=True):
