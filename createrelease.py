68db3d980d96e3df204299c7a9b0215d8c47fe32
Skye <105599773+skyelynwaddell@users.noreply.github.com>
2024-03-03 10:53:25 -0700

    hazy pack

--- createrelease.py
+++ createrelease.py
@@ -1,16 +1,16 @@
 import fileinput
 
 replaceTexts = {
-    '#COVER_ART':'https://s3.amazonaws.com/gather.fandalism.com/3000x3000%2D1197190%2D%2DD7A2CFEA%2D0A0B%2D4D2D%2D80581A9E82C13913%2D%2D0%2D%2D1696534%2D%2Dver2iongoodprod.9dmbeats.jpg',
-    '#TITLE':'ver2ion - good',
+    '#COVER_ART':'https://s3.amazonaws.com/gather.fandalism.com/1197190%2D%2D42B14099%2DB251%2D4CB5%2DA5F4EA7105CDF345%2D%2D0%2D%2D1862864%2D%2Dver2ioncantholdmebackprod.ymvu.wav.png',
+    '#TITLE':'ver2ion - hazy pack',
     '#ARTIST':'ver2ion',
-    '#SONG_TITLE':'good',
-    '#RELEASE_DATE':'February 18 2024',
+    '#SONG_TITLE':'hazy pack',
+    '#RELEASE_DATE':'March 3 2024',
 
-    '#SOUNDCLOUD':'',
+    '#SOUNDCLOUD':'https://soundcloud.com/ver2ion/ver2ion-hazy-pack-prod-ymvuwav-1',
     '#SPOTIFY':'',
     '#ITUNES':'',
-    '#YOUTUBE':'',
+    '#YOUTUBE':'https://www.youtube.com/watch?v=xQjVEMtVpWk',
     '#YOUTUBE_MUSIC':'',
     '#DEEZER':'',
 }
